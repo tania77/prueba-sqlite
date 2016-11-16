dab31a47e96f8365b38eaacac050ca097cb6d74b
alu0100786330 <alu0100786330@ull.edu.es>
2016-11-16 13:39:24 +0000

    actualizando passport.js

---
+++ bin/passport.js
@@ -0,0 +1,35 @@
+'use strict';
+var inquirer = require('inquirer');
+
+var questions = [
+  {
+    type: 'input',
+    name: 'first_name',
+    message: 'What\'s your first name'
+  },
+  {
+    type: 'input',
+    name: 'last_name',
+    message: 'What\'s your last name',
+    default: function () {
+      return 'Doe';
+    }
+  },
+  {
+    type: 'input',
+    name: 'phone',
+    message: 'What\'s your phone number',
+    validate: function (value) {
+      var pass = value.match(/^\d+$/i);
+      if (pass) {
+        return true;
+      }
+
+      return 'Please enter a valid phone number';
+    }
+  }
+];
+
+inquirer.prompt(questions).then(function (answers) {
+  console.log(JSON.stringify(answers, null, '  '));
+});
